5981626337fcdc0fd022fc8df205f358883ca0bc
burakiscoding <burak.kurtarir35@hotmail.com>
2024-08-19 13:56:31 +0300

    Initial Commit

---
+++ DesignExamples/DesignExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignExamplesApp.swift
+//  DesignExamples
+//
+//  Created by Burak Kurtarır on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
